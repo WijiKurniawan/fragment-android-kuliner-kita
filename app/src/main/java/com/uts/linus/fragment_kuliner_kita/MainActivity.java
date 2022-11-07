0d8ef706b5ed9fe20d1ef093205901723e8aaf93
Linus <wijikurniawan121@gmail.com>
2022-11-07 15:13:05 +0700

    perbaiki lgii

--- app/src/main/java/com/uts/linus/fragment_kuliner_kita/MainActivity.java
+++ app/src/main/java/com/uts/linus/fragment_kuliner_kita/MainActivity.java
@@ -1,8 +1,10 @@
 package com.uts.linus.fragment_kuliner_kita;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreatePanelMenu(Menu menu) {
+        get
+        return super.onCreatePanelMenu(menu);
     }
 }
